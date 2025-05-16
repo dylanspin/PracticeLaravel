4b1c90a154b5b826d18cd09307d5adca07173832
dylanspin <47520114+dylanspin@users.noreply.github.com>
2025-05-17 00:19:26 +0200

    started learning again

---
+++ learningProject/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome'); // returns the welcome.blade.php file 
+});
+
+
+Route::get('/aboutstring', function () { // if page is /aboutstring 
+    return 'about page but just a string'; // returns just the string which some how works
+});
+
+Route::get('/aboutarray', function () { // if page is /aboutstring 
+    return ['something', 'second item', 'some third item', 'this reminds me of drenthe college']; // returns just the array 
+});
+
+
+Route::get('/about', function () { // if page is /about 
+    return view('about'); // returns the welcome.blade.php file 
+});
